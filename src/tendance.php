7e4058b454d509aecb07118f98bb6f5aa8d214f8
cy-fa <cypher_fab@proton.me>
2024-06-10 16:59:59 +0200

    v9

--- src/tendance.php
+++ src/tendance.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tendance</title>
-   <link rel="stylesheet" href="./style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tendance</title>
+  <link rel="stylesheet" href="./style.css">
 </head>
+
 <body>
-<nav class="navbar dark-mode" role="navigation">
+  <nav class="navbar dark-mode" role="navigation">
     <div class="navbar__logo">
       <p>F&K</p>
     </div>
@@ -15,8 +17,8 @@
       <li class="navbar__link first"><a href="#">TENDANCES</a></li>
       <li class="navbar__link second"><a href="#section2">CATALOGUE</a></li>
       <li class="navbar__link third"><a href="#"><img src="./img/utilisateur.png" width=" 35px" heigh="35px" alt=""></a></li>
-      <li class="navbar__link four"><a href="#"><img src="./img/retirer-du-panier.png"width=" 35px" heigh="35px"  alt=""></a></li>
-      
+      <li class="navbar__link four"><a href="#"><img src="./img/retirer-du-panier.png" width=" 35px" heigh="35px" alt=""></a></li>
+
     </ul>
     <button class="burger">
       <span class="bar"></span>
@@ -25,16 +27,18 @@
 
 
   <section>
-<div class="contenaire_tendance">
-  
-    <a href=""><img src="./img/tendance1.jpg" alt=""></a>
-    <a href=""><img src="./img/tendance2.jpg" alt=""></a>
-    <a href=""><img src="./img/tendance3.jpg" alt=""></a>
-</div>
+    <div class="contenaire_tendance">
+      <div class="contenaire_photo">
+        <a href=""><img src="./img/tendance1.jpg" height="600px" width="450px" alt=""></a>
+        <a href=""><img src="./img/tendance2.jpg" height="600px" width="450px" alt=""></a>
+        <a href=""><img src="./img/tendance3.jpg" height="600px" width="450px" alt=""></a>
+      </div>
+    </div>
 
 
 
   </section>
-  <script src="script.js"></script>  
+  <script src="script.js"></script>
 </body>
+
 </html>
