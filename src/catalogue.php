d7ba86a787a364d4f35250b0973a1c16641b7687
KristineManu <kristinarman21@gmail.com>
2024-06-11 11:12:42 +0200

    V13

--- src/catalogue.php
+++ src/catalogue.php
@@ -4,17 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<?php
+  include './element/navbar.php';
+  ?>
     <div class="carte">
+        <div class="divIMG">
         <div class="titre"> Tee-Shirt Fantaisie "Wild Free"<hr></div>
         <button class="btnSection"> 
-<a href="produit.php">Ajouter</a>
+         <a href="produit.php">Ajouter</a>
         </button>
 
 
-
+        </div>
     </div>
-    
+    <script src="script.js"></script>  
 </body>
 </html>
