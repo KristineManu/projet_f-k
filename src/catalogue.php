ef37e4e7934fae4087edd516610a9a02a2c95f68
KristineManu <kristinarman21@gmail.com>
2024-06-11 15:04:34 +0200

    v16

--- src/catalogue.php
+++ src/catalogue.php
@@ -7,19 +7,36 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<?php
+  <?php
   include './element/navbar.php';
-  ?>
-    <div class="carte">
-        <div class="divIMG">
-        <div class="titre"> Tee-Shirt Fantaisie "Wild Free"<hr></div>
-        <button class="btnSection"> 
-         <a href="produit.php">Ajouter</a>
-        </button>
+   ?>
+ <div class="contenaire_catalogue">
 
+    <div class="container_items">
+      <div class="item item1">
+            <div class="divIMG"><img src="../img/product/echapee_sauvage_1a.jpg" width="450px" height="600px" alt=""></div>
+            
+            <div class="titre"> Tee-Shirt Fantaisie "Wild Free"</div>
+            <div class="prix"> 29.99€</div>
+            
+            <button class="btn"><a href="produit.php"></a>
 
+            </button>
+        </div>
+        <div class="item item2">
+            <div class="divIMG"><img src="../img/product/echapee_sauvage_2a.jpg" width="450px" height="600px" alt=""></div>
+        <div class="titre"> Tee-Shirt Fantaisie "Wild Free"</div>
+            <div class="prix"> 29.99€</div>
+            <button class="btn"><a href="produit.php"></a></button>
+        </div>
+        <div class="item item3">
+            <div class="divIMG"><img src="../img/product/echapee_sauvage_3a.jpg" width="450px" height="600px" alt=""></div>
+            <div class="titre"> Tee-Shirt Fantaisie "Wild Free"</div>
+            <div class="prix"> 29.99€</div>
+            <button class="btn"><a href="produit.php"></a></button>
         </div>
     </div>
+</div>
     <script src="script.js"></script>  
 </body>
 </html>
