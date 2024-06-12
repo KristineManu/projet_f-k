1e725f3ac898bd4f1d5cb6cf4d750a74f8ecc7a5
cy-fa <cypher_fab@proton.me>
2024-06-12 14:22:12 +0200

    v2.2

--- src/index.php
+++ src/index.php
@@ -47,19 +47,20 @@
           <span class="sr-only">Next</span>
         </a>
       </div>
-      </section>
+    </div>
+    </section>
 
-      <?php
-      include './element/footer.php';
-      ?>
+    <?php
+    include './element/footer.php';
+    ?>
 
-      <!-- Bootstrap JS, Popper.js, and jQuery -->
-      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-      
-      
-  
+    <!-- Bootstrap JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+
 
 
 </body>
