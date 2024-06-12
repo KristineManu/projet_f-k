4b90e62a03aaafa61a6bc52cb23c74297810cce6
KristineManu <kristinarman21@gmail.com>
2024-06-12 13:36:14 +0200

    v16

--- src/element/footer.php
+++ src/element/footer.php
@@ -1,3 +1,18 @@
 <footer>
-    <h2>je suis un footer</h2>
+    <div class="wrapper">
+        <div class="footer_top_left">
+            <h2>Paiement en ligne</h2>
+            <p>Livraison directement à domicile</p>
+        </div>
+        <div class="footer_top_right">
+            <h2>Paiement en ligne</h2>
+            <p>Livraison directement à domicile</p>
+        </div>
+    </div>
+    
+    <div class="footer_bottom">
+
+
+    </div>
+  
 </footer>
