62a7c2858c71d6cb3a5c261cb45d3bcfa173b8a9
KristineManu <kristinarman21@gmail.com>
2024-06-17 10:45:21 +0200

    footter términer

--- src/element/footer.php
+++ src/element/footer.php
@@ -7,22 +7,21 @@
             </div>
             <div class="footer-elements">
 
-               
-                <div class="block"><img src="img/icon/01-COMMANDEZ.svg" height="100px" width="100px">
+                <div class="bloc">
+                    <div class="img"><img src="img/icon/01-COMMANDEZ.svg" height="100px" width="100px"></div>
                     <p>Choisissez vos articles et <br>validez votre panier</p>
                 </div>
-            
-                <div class="block"><img src="img/icon/02-PAIEMENT.svg" height="100px" width="100px" alt="">
+                <div class="bloc">
+                    <div class="img"><img src="img/icon/02-PAIEMENT.svg" height="100px" width="100px" alt=""></div>
                     <p>Payer vos articles <br>
                         par Carte Bleue</p>
                 </div>
-                
-                <div class="block"><img src="img/icon/03-LIVRAISON.svg" height="100px" width="100px" alt="">
+                <div class="bloc">
+                    <div class="img"><img src="img/icon/03-LIVRAISON.svg" height="100px" width="100px" alt=""></div>
                     <p>Livraison colissimo <br>offerte pour 60€ d'achat</p>
                 </div>
-    
-            
-                <div class="block"><img src="img/icon/04-RETOURS.svg" height="100px" width="100px" alt="">
+                <div class="bloc">
+                    <div class="img"><img src="img/icon/04-RETOURS.svg" height="100px" width="100px" alt=""></div>
                     <p>Retours sous 30j <br>avec colissimo</p>
                 </div>
             </div>
@@ -34,22 +33,25 @@
                 <p>Payez quand vous l'avez !</p>
             </div>
             <div class="footer-elements">
-             
-                <div class="block"><img src="img/icon/01-COMMANDEZ.svg" height="100px" width="100px">
-                    <p>Commandez <br>vos articles</p>
+               
+                <div class="bloc">
+                        <div class="img"><img src="img/icon/01-COMMANDEZ.svg" height="100px" width="100px"></div>
+                        <p>Commandez <br>vos articles</p>
                 </div>
-                
-                <div class="block"><img src="img/icon/point-de-retrait.svg" height="100px" width="100px" alt="">
-                    <p>Choisissez <br>votre point de retrait</p>
+                <div class="bloc">
+                        <div class="img"><img src="img/icon/point-de-retrait.svg" height="100px" width="100px" alt=""></div>
+                        <p>Choisissez <br>votre point de retrait</p>
                 </div>
+            
                 
-                <div class="block"><img src="img/icon/livraison.svg" height="100px" width="100px" alt="">
-                    <p>Profitez de la livraison <br>et des retours gratuits</p>
+                <div class="bloc">
+                        <div class="img"><img src="img/icon/livraison.svg" height="100px" width="100px" alt=""></div>
+                        <p>Profitez de la livraison <br>et des retours gratuits</p>
                 </div>
-                
-                <div class="block"><img src="img/icon/retirez-et-payez-en-magasin.svg" height="100px" width="100px" alt="">
-                    
-                    <p>Retirez et payez <br>vos articles en magasin</p>
+                <div class="bloc">
+                        <div class="img"><img src="img/icon/retirez-et-payez-en-magasin.svg" height="100px" width="100px" alt=""></div>
+                        <p>Retirez et payez <br>vos articles en magasin</p>
+                </div>
                 </div>
             </div>
         </div>
